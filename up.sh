8cd9750578feafed26628dc26ed22af11568d910
Gustavo Carrança <gustavo.carranca1@hotmail.com>
2023-04-14 16:54:45 -0300

    Alterando comandos do github actions

--- up.sh
+++ up.sh
@@ -2,19 +2,24 @@
 
 # executar "bash up.sh"
 
-echo -n "Qual ambiente você quer subir? API ou WEB? Digite: "
-read env
+# echo -n "Qual ambiente você quer subir? API ou WEB? Digite: "
+# read env
 
-if [ -z "$env" ]; then
-  echo "Opção inválida, você precisa digitar alguma opção! O seu ambiente não foi alterado."
-  exit 0
-elif [ $env == "API" ]; then
-  echo "---> Subindo a API <---"
-  cd ./apps/api && yarn dev
-elif [ $env == "WEB" ]; then
-  echo "---> Subindo a Web Application <---"
-  cd ./apps/web && yarn dev
-else
-  echo "Opção inválida, digite na próxima vez 'API' ou 'WEB'. O seu ambiente não foi alterado."
-  exit 0
-fi
+# if [ -z "$env" ]; then
+#   echo "Opção inválida, você precisa digitar alguma opção! O seu ambiente não foi alterado."
+#   exit 0
+# elif [ $env == "API" ]; then
+#   echo "---> Subindo a API <---"
+#   cd ./apps/api && yarn dev
+# elif [ $env == "WEB" ]; then
+#   echo "---> Subindo a Web Application <---"
+#   cd ./apps/web && yarn dev
+# else
+#   echo "Opção inválida, digite na próxima vez 'API' ou 'WEB'. O seu ambiente não foi alterado."
+#   exit 0
+# fi
+
+echo "---> Subindo a API <---"
+cd ./apps/api && yarn dev
+echo "---> Subindo a Web Application <---"
+cd ./apps/web && yarn dev
